b0eb9add31bf9034dd05d940183d74faf7c5916c
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-06 15:38:57 +0100

    Getting a user by id

--- src/main/java/pt/nandes/workshopmongo/resources/UserResource.java
+++ src/main/java/pt/nandes/workshopmongo/resources/UserResource.java
@@ -3,15 +3,14 @@ package pt.nandes.workshopmongo.resources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pt.nandes.workshopmongo.domain.User;
 import pt.nandes.workshopmongo.dto.UserDTO;
 import pt.nandes.workshopmongo.services.UserService;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -25,5 +24,11 @@ public class UserResource {
         List<User> users = service.findAll();
         List<UserDTO> userDto = users.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(userDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok(new UserDTO(obj));
     }
 }
---
+++ src/main/java/pt/nandes/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package pt.nandes.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
