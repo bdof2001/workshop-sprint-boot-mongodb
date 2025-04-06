064c3cb8f154b5d1659257c4ac255ce81ce96a80
Bruno Fernandes <1191264@isep.ipp.pt>
2025-04-06 15:56:45 +0100

    User input with POST

--- src/main/java/pt/nandes/workshopmongo/services/UserService.java
+++ src/main/java/pt/nandes/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package pt.nandes.workshopmongo.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pt.nandes.workshopmongo.domain.User;
+import pt.nandes.workshopmongo.dto.UserDTO;
 import pt.nandes.workshopmongo.repository.UserRepository;
 import pt.nandes.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
